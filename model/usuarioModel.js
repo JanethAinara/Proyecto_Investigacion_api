32641e944fe747c062e8dc04807ad44c7fafe6ce
JanethAinara <janeth_escobarg@yahoo.es>
2021-12-05 06:15:32 -0500

    Add model user

---
+++ model/usuarioModel.js
@@ -0,0 +1,28 @@
+const {Schema, model} = require('mongoose')
+
+const usuario = new Schema ({
+
+    nombre: {
+        type: String,
+        required: true
+    },
+    identificacion: {
+        type: Number,
+        unique: true,
+        required: true
+    },
+    perfil: {
+        type: String,
+        required: true
+    },
+    estado: {
+        type:String,
+        default: "Inactivo"
+    },
+    clave: {
+        type: String,
+        required: true
+    }
+})    
+
+module.exports = model ('usuario', usuario)
